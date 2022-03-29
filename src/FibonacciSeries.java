5bf52346de2f4066408699af9feb49b91d2fd034
palashp94 <palashmadney@gmail.com>
2022-03-29 16:08:43 +0530

    Added:CalculatedDistinctCouponNumber

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -4,16 +4,13 @@ public class FibonacciSeries {
         int n = 100;
         int a = 0;
         int b = 1;
-
         System.out.println("Fibonacci Series Upto " + n + ": ");
 
         while (a <= n) {
             System.out.print(a + ", ");
-
             int nextTerm = a + b;
             a = b;
             b = nextTerm;
-
         }
     }
 }
